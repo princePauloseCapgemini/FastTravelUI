4fcd7371c35d5ed08cb172f312d3fc31b88a83c5
Paulose <prince.paulose@capgemini.com>
2023-05-05 12:54:57 +0530

    integrate graphql apollo client

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 import "./index.css";
 
+const client = new ApolloClient({
+  uri: "",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
