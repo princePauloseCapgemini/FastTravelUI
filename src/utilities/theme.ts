76f77e99a4012ee3c8d6fbe67d483a288297ce88
Paulose <prince.paulose@capgemini.com>
2023-05-05 14:01:29 +0530

    configure theme

---
+++ src/utilities/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  primary: "#424a54",
+  highlight: "#cddc39",
+  back: "#f5f5f5",
+  white: "#fff",
+  label: "#4d4d4d",
+  black: "#000",
+  widget: "#F2F4F6",
+};
+
+const theme = extendTheme({ colors });
+
+export default theme;
