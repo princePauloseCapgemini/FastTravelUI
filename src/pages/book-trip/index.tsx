f96e43d181074bf90a1369b10832a0a5d9eb3efc
Paulose <prince.paulose@capgemini.com>
2023-05-08 16:36:14 +0530

    integrate google maps

--- src/pages/book-trip/index.tsx
+++ src/pages/book-trip/index.tsx
@@ -1,15 +1,13 @@
 import {
   Box,
   Button,
-  ButtonGroup,
-  Flex,
-  HStack,
-  IconButton,
+  VStack,
   Input,
   SkeletonText,
+  SimpleGrid,
   Text,
+  HStack,
 } from "@chakra-ui/react";
-import { FaLocationArrow, FaTimes } from "react-icons/fa";
 
 import {
   useJsApiLoader,
@@ -19,131 +17,131 @@ import {
   DirectionsRenderer,
 } from "@react-google-maps/api";
 import { useRef, useState } from "react";
+import { useIsMobile } from "../../utilities/isMobile";
 
 const center = { lat: 48.8584, lng: 2.2945 };
 
 function BookTrip() {
+  const { isMobile } = useIsMobile();
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: "AIzaSyDy5h767NqUH-waT-5hGRPkxCaGERJLI5w",
+    googleMapsApiKey: "AIzaSyBoVQ3bKj_EczoAexCIEwqc85dNdvKKuX8",
     libraries: ["places"],
   });
 
-  const [map, setMap] = useState(/** @type google.maps.Map */ null);
-  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [map, setMap] = useState<any>(null);
+  const [directionsResponse, setDirectionsResponse] = useState<any>(null);
   const [distance, setDistance] = useState("");
   const [duration, setDuration] = useState("");
 
-  /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef();
-  /** @type React.MutableRefObject<HTMLInputElement> */
-  const destiantionRef = useRef();
+  const originRef = useRef<HTMLInputElement>(null);
+  const destiantionRef = useRef<HTMLInputElement>(null);
 
   if (!isLoaded) {
     return <SkeletonText />;
   }
 
   async function calculateRoute() {
-    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+    if (
+      originRef?.current?.value === "" ||
+      destiantionRef?.current?.value === ""
+    ) {
       return;
     }
     const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destiantionRef.current.value,
+      origin: originRef?.current?.value || "",
+      destination: destiantionRef?.current?.value || "",
       travelMode: google.maps.TravelMode.DRIVING,
     });
-    setDirectionsResponse(results);
-    setDistance(results.routes[0].legs[0].distance.text);
-    setDuration(results.routes[0].legs[0].duration.text);
+    setDirectionsResponse(results || null);
+    setDistance(results?.routes?.[0]?.legs?.[0]?.distance?.text || "");
+    setDuration(results?.routes?.[0]?.legs?.[0]?.duration?.text || "");
   }
 
   function clearRoute() {
     setDirectionsResponse(null);
     setDistance("");
     setDuration("");
-    originRef.current.value = "";
-    destiantionRef.current.value = "";
+    if (originRef?.current) {
+      originRef.current.value = "";
+    }
+    if (destiantionRef?.current) {
+      destiantionRef.current.value = "";
+    }
   }
 
   return (
-    <Flex
-      position="relative"
-      flexDirection="column"
-      alignItems="center"
-      h="100%"
-      w="100%"
-      bgColor="grey"
-    >
-      <Box position="absolute" left={0} top={0} h="100%" w="100%">
-        <GoogleMap
-          center={center}
-          zoom={15}
-          mapContainerStyle={{ width: "100%", height: "100%" }}
-          options={{
-            zoomControl: false,
-            streetViewControl: false,
-            mapTypeControl: false,
-            fullscreenControl: false,
-          }}
-          onLoad={(map) => setMap(map)}
+    <Box w="100%" minH="100%" bgColor="grey" px="8" py="4">
+      <SimpleGrid columns={isMobile ? 1 : 2}>
+        <Box
+          p={4}
+          borderRadius="lg"
+          m={4}
+          bgColor="white"
+          shadow="base"
+          zIndex="1"
         >
-          <Marker position={center} />
-          {directionsResponse && (
-            <DirectionsRenderer directions={directionsResponse} />
-          )}
-        </GoogleMap>
-      </Box>
-      <Box
-        p={4}
-        borderRadius="lg"
-        m={4}
-        bgColor="white"
-        shadow="base"
-        minW="container.md"
-        zIndex="1"
-      >
-        <HStack spacing={2} justifyContent="space-between">
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input type="text" placeholder="Origin" ref={originRef} />
-            </Autocomplete>
-          </Box>
-          <Box flexGrow={1}>
-            <Autocomplete>
-              <Input
-                type="text"
-                placeholder="Destination"
-                ref={destiantionRef}
-              />
-            </Autocomplete>
-          </Box>
+          <VStack
+            w="100%"
+            alignItems="flex-start"
+            spacing={4}
+            justifyContent="space-between"
+          >
+            <Box flexGrow={1} w="100%">
+              <Autocomplete>
+                <Input type="text" placeholder="Origin" ref={originRef} />
+              </Autocomplete>
+            </Box>
+            <Box flexGrow={1} w="100%">
+              <Autocomplete>
+                <Input
+                  type="text"
+                  placeholder="Destination"
+                  ref={destiantionRef}
+                />
+              </Autocomplete>
+            </Box>
 
-          <ButtonGroup>
-            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
-              Calculate Route
-            </Button>
-            <IconButton
-              aria-label="center back"
-              icon={<FaTimes />}
-              onClick={clearRoute}
-            />
-          </ButtonGroup>
-        </HStack>
-        <HStack spacing={4} mt={4} justifyContent="space-between">
-          <Text>Distance: {distance} </Text>
-          <Text>Duration: {duration} </Text>
-          <IconButton
-            aria-label="center back"
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => {
-              map.panTo(center);
-              map.setZoom(15);
+            <HStack justifyContent="space-between" w="100%">
+              <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
+                Calculate Route
+              </Button>
+              <Button onClick={clearRoute}>Clear</Button>
+            </HStack>
+          </VStack>
+          <VStack w="100%" alignItems="flex-start" spacing={4} mt={4}>
+            {distance && <Text>Distance: {distance} </Text>}
+            {duration && <Text>Duration: {duration} </Text>}
+          </VStack>
+        </Box>
+        <Box
+          borderRadius="lg"
+          m={4}
+          bgColor="white"
+          shadow="base"
+          zIndex="1"
+          overflow="hidden"
+        >
+          <GoogleMap
+            center={center}
+            zoom={15}
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            options={{
+              zoomControl: false,
+              streetViewControl: false,
+              mapTypeControl: false,
+              fullscreenControl: false,
             }}
-          />
-        </HStack>
-      </Box>
-    </Flex>
+            onLoad={(map: any) => setMap(map)}
+          >
+            <Marker position={center} />
+            {directionsResponse && (
+              <DirectionsRenderer directions={directionsResponse} />
+            )}
+          </GoogleMap>
+        </Box>
+      </SimpleGrid>
+    </Box>
   );
 }
 
